06cb02ad79cffb6d5e25716c93ff76b5c31be61a
GURUBHARAN N M <78141060+GURUNMG@users.noreply.github.com>
2022-06-26 20:18:18 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// console.log("Welcome to the file system.");
-
-const fs=require('fs');
-
-console.log("Writing the file by using file system...");
-fs.writeFileSync('D:/BACKEND/VIDEO9(fs)/Welcome.txt', 'Welcome to the file system.');
