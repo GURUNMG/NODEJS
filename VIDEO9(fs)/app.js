31a348b41a57e2eb52494d9b91965e8f98306b59
GURUBHARAN N M <78141060+GURUNMG@users.noreply.github.com>
2022-06-26 19:58:36 +0530

    FILE_SYSTEM

---
+++ VIDEO9(fs)/app.js
@@ -0,0 +1,6 @@
+// console.log("Welcome to the file system.");
+
+const fs=require('fs');
+
+console.log("Writing the file by using file system...");
+fs.writeFileSync('D:/BACKEND/VIDEO9(fs)/Welcome.txt', 'Welcome to the file system.');
