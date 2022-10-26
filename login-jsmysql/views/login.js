d54a2d7658ef5b2fbd49b93e4943d21deeedd6fb
GURUBHARAN N M <78141060+GURUNMG@users.noreply.github.com>
2022-10-26 17:41:24 +0530

    Login form with mysql

---
+++ login-jsmysql/views/login.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+ <link rel="stylesheet" href="/style.css">
+ {{!--  <link rel="stylesheet" href="../public/style.css"> --}} 
+ {{!-- above comment line does not allows us to locate the css file ... --}}
+ {{!-- but by default "/"==> locate to public directory --}}
+ <title>home page</title>
+</head>
+<body>
+ <h1>HOME PAGE of HBS</h1>
+
+ <nav>
+  <h3> NODE MySQL</h3>
+  <ul>
+   <li><a href="/">HOME</a></li>
+   <li><a href="/login">LOGIN</a> </li>
+   <li><a href="/register">REGISTER</a></li>
+  </ul>
+ </nav>
+
+<div class="container mt-4">
+ <div class="jumbotron">
+  <h1 class="display-4">LOGIN PROJECT</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+ </div>
+</div>
+ <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+{{!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> --}}
+</body>
+</html>
